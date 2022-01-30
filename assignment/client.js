7244c89d3a7a170e652423f4e4765cecef3d0fc0
Clay Webb <clay_webb@outlook.com>
2022-01-29 18:09:16 -0600

    This will display the correct total monthly wages

--- assignment/client.js
+++ assignment/client.js
@@ -1,6 +1,7 @@
 $(document).ready(readyNow);
 
 let employees = []; // This is declaring a global 'employees' empty array
+let monTotal = 0;
 
 function newEmployee (fNameInput, lNameInput, idInput, titleInput, salaryInput) {
     console.log('In newEmployee:', fNameInput, lNameInput, idInput, titleInput, salaryInput);
@@ -18,6 +19,7 @@ function newEmployee (fNameInput, lNameInput, idInput, titleInput, salaryInput) 
 function readyNow() {
     console.log('jQuery is working');
     $('.submitButton').on('click', addEmployee);
+    // $('.submitButton').on('click', monthlyTotal);
 } // End readyNow function
 
 function addEmployee(){
@@ -34,21 +36,32 @@ function addEmployee(){
     $('#idField').val('');
     $('#titleField').val('');
     $('#annualSalaryField').val('');
-    $('.submitButton').on('click', displayEmp);
+    // $('.submitButton').on('click', displayEmp);
     displayEmp();
     return true;
 } // End addEmployee function
 
 function displayEmp() {
     console.log('In displayEmp');
+    monTotal = 0;
     $('#theEmpList').empty();
     for (let emp of employees){
         $('#theEmpList').append(`<tr><td>${emp.first}</td><td>${emp.last}</td><td>${emp.id}</td><td>${emp.title}</td><td>${emp.salary}</td><td><button>${'Delete'}</button></td></tr>`);
+        monthlyTotal(emp.salary);
     }; // End for of loop
-    
-
     
 } // End displayEmp function
 
+function monthlyTotal(salary){
+    console.log('In monthlyTotal');
+    $('#monthly-total').empty(); // This will clear the Total Monthly on the DOM
+    // salary /= 12;
+    monTotal += salary / 12;
+    $('#monthly-total').append(Math.round(monTotal));
+
+    // return monTotal;
+    console.log('In monthlyTotal:', monTotal);
+}
 // MONTHLY TOTAL IS DIVIDE BY 12
 
+// console.log('In monthlyTotal:', monTotal);
